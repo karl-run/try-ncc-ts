09558aef4cb0d1a2e5d0887dfd57fe7b72e604fe
Karl J. Overå <k@rl.run>
2018-12-08 13:05:41 +0100

    Test with a dependency

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
+import fetch from "node-fetch";
+
 interface Oof {
-    name: string,
-    gnelf: boolean,
-    gnoblin: boolean,
-    gnome: boolean,
+  name: string;
+  gnelf: boolean;
+  gnoblin: boolean;
+  gnome: boolean;
 }
 
 const theBoi: Oof = {
-    name: "Red",
-    gnelf: false,
-    gnoblin: false,
-    gnome: true
-}
+  name: "Red",
+  gnelf: false,
+  gnoblin: false,
+  gnome: true
+};
 
-console.log("Hello: ", theBoi)
-console.log("Args:", process.argv.slice(2))
+console.log("Hello: ", theBoi);
+console.log("Args:", process.argv.slice(2));
+
+console.log("Fetching...");
+fetch("https://meme-slash.now.sh")
+  .then(res => res.json())
+  .then(response => {
+    console.log("Response: ", response);
+  });
